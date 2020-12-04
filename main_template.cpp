16a4d4893168adbedc38a0397240c41db6d3b1b7
ve6ji-code <56846893+ve6ji-code@users.noreply.github.com>
2020-12-04 04:45:03 -0700

    Create main_template.cpp

---
+++ main_template.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(int argc, char const *argv[]) {
+  try 
+  {
+    
+  }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+  return 0;
+}
